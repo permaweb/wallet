0999bbb2a57abfbd13ea73226feb296c59f223de
mayurmarvel <45968855+mayurmarvel@users.noreply.github.com>
2024-03-10 11:10:20 +0000

    prepending chars fix

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 #!/usr/bin/env node
 
-require('arweave').init({}).wallets.generate().then(JSON.stringify).then(console.log.bind(console))
+const fs = require('fs');
+const Arweave = require('arweave');
+
+async function generateWallet(walletName) {
+    const arweave = Arweave.init({});
+    const wallet = await arweave.wallets.generate();
+    const walletJSON = JSON.stringify(wallet);
+
+    fs.writeFileSync(walletName, walletJSON);
+    console.log(`Wallet generated and saved as \x1b[34m${walletName}\x1b[0m`); // Highlighting walletName in blue
+}
+
+async function main() {
+    if (process.argv.length >= 3) {
+        const walletName = process.argv[2];
+        if (walletName.endsWith('.json')) {
+            await generateWallet(walletName);
+        } else {
+            console.log('Invalid wallet name. Wallet name must end with ".json"');
+        }
+    } else {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Enter the wallet name (ending with ".json"): ', async (walletName) => {
+            readline.close();
+            if (walletName.endsWith('.json')) {
+                await generateWallet(walletName);
+            } else {
+                console.log('Invalid wallet name. Wallet name must end with ".json"');
+            }
+        });
+    }
+}
+
+main();
